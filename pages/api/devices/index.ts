95c01ba794a4bbf944e84611db77dac4ffb729e1
bryanorellana28 <63586658+bryanorellana28@users.noreply.github.com>
2025-07-04 14:30:39 -0600

    Add inventory CRUD with Chakra UI

---
+++ pages/api/devices/index.ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../../lib/prisma'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    const devices = await prisma.device.findMany()
+    return res.status(200).json(devices)
+  }
+
+  if (req.method === 'POST') {
+    const {
+      ipGestion,
+      nombre,
+      sitio,
+      rack,
+      tipoEquipo,
+      marca,
+      modelo,
+      versionSoftware,
+      serial
+    } = req.body
+
+    const device = await prisma.device.create({
+      data: {
+        ipGestion,
+        nombre,
+        sitio,
+        rack,
+        tipoEquipo,
+        marca,
+        modelo,
+        versionSoftware,
+        serial
+      }
+    })
+    return res.status(201).json(device)
+  }
+
+  res.status(405).json({ message: 'Method not allowed' })
+}
