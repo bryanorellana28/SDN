3f0703cfe0b736fe78589ec88ea8ed0d297658aa
bryanorellana28 <63586658+bryanorellana28@users.noreply.github.com>
2025-07-07 12:08:38 -0600

    Improve device info view using Chakra UI

--- pages/inventory/[id].tsx
+++ pages/inventory/[id].tsx
@@ -1,6 +1,14 @@
 import { GetServerSideProps } from 'next'
 import { getSession } from 'next-auth/react'
-import { Box, Heading, Text, List, ListItem } from '@chakra-ui/react'
+import {
+  Box,
+  Heading,
+  Text,
+  List,
+  ListItem,
+  Stack,
+  Flex
+} from '@chakra-ui/react'
 import SidebarLayout from '../../components/SidebarLayout'
 import { prisma } from '../../lib/prisma'
 
@@ -24,25 +32,67 @@ export default function ViewDevice({ device }: { device: Device }) {
     <SidebarLayout>
       <Box maxW='md' mx='auto'>
         <Heading size='md' mb={4}>Información del Dispositivo</Heading>
-        <Text><b>IP:</b> {device.ipGestion}</Text>
-        <Text><b>Sitio:</b> {device.sitio}</Text>
-        <Text><b>Rack:</b> {device.rack}</Text>
-        <Text><b>Tipo:</b> {device.tipoEquipo}</Text>
-        <Text><b>Marca:</b> {device.marca}</Text>
-        {device.hostname && <Text><b>Hostname:</b> {device.hostname}</Text>}
-        {device.versionSoftware && <Text><b>Versión:</b> {device.versionSoftware}</Text>}
-        {device.cpu && <Text><b>CPU:</b> {device.cpu}</Text>}
-        {device.boardName && <Text><b>Board:</b> {device.boardName}</Text>}
-        {device.interfaces.length > 0 && (
-          <Box mt={4}>
-            <Heading size='sm'>Interfaces</Heading>
-            <List spacing={1} mt={2}>
-              {device.interfaces.map((i) => (
-                <ListItem key={i.id}>{i.name}{i.description ? ` - ${i.description}` : ''}</ListItem>
-              ))}
-            </List>
-          </Box>
-        )}
+        <Box bg='white' p={4} borderRadius='md' boxShadow='md'>
+          <Stack spacing={3}>
+            <Flex>
+              <Text fontWeight='bold' w='40%'>IP:</Text>
+              <Text w='60%'>{device.ipGestion}</Text>
+            </Flex>
+            <Flex>
+              <Text fontWeight='bold' w='40%'>Sitio:</Text>
+              <Text w='60%'>{device.sitio}</Text>
+            </Flex>
+            <Flex>
+              <Text fontWeight='bold' w='40%'>Rack:</Text>
+              <Text w='60%'>{device.rack}</Text>
+            </Flex>
+            <Flex>
+              <Text fontWeight='bold' w='40%'>Tipo:</Text>
+              <Text w='60%'>{device.tipoEquipo}</Text>
+            </Flex>
+            <Flex>
+              <Text fontWeight='bold' w='40%'>Marca:</Text>
+              <Text w='60%'>{device.marca}</Text>
+            </Flex>
+            {device.hostname && (
+              <Flex>
+                <Text fontWeight='bold' w='40%'>Hostname:</Text>
+                <Text w='60%'>{device.hostname}</Text>
+              </Flex>
+            )}
+            {device.versionSoftware && (
+              <Flex>
+                <Text fontWeight='bold' w='40%'>Versión:</Text>
+                <Text w='60%'>{device.versionSoftware}</Text>
+              </Flex>
+            )}
+            {device.cpu && (
+              <Flex>
+                <Text fontWeight='bold' w='40%'>CPU:</Text>
+                <Text w='60%'>{device.cpu}</Text>
+              </Flex>
+            )}
+            {device.boardName && (
+              <Flex>
+                <Text fontWeight='bold' w='40%'>Board:</Text>
+                <Text w='60%'>{device.boardName}</Text>
+              </Flex>
+            )}
+          </Stack>
+          {device.interfaces.length > 0 && (
+            <Box mt={4}>
+              <Heading size='sm' mb={2}>Interfaces</Heading>
+              <List spacing={1}>
+                {device.interfaces.map((i) => (
+                  <ListItem key={i.id}>
+                    {i.name}
+                    {i.description ? ` - ${i.description}` : ''}
+                  </ListItem>
+                ))}
+              </List>
+            </Box>
+          )}
+        </Box>
       </Box>
     </SidebarLayout>
   )
